7cc96bc3477911b65c59c4a1c11a7d574466d1c5
asliddin2002developer <eshonkulovasliddindeveloper@gmail.com>
2022-11-19 10:46:48 +0500

    switch final tasks

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -3,5 +3,46 @@ package school.mjc.stage0.conditions.finalTask;
 public class DaysInMonth {
     public void printDays(int year, int month) {
 
+        if ((year > 0) && (month > 0 && month < 13)) {
+            switch (month) {
+                case 2:
+                    boolean isYearLeap = isLeapYear(year);
+                    String s = isYearLeap ? "29" : "28";
+                    System.out.println(s);
+                    break;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    System.out.println("30");
+                    break;
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    System.out.println("31\n");
+            }
+        }else {
+            System.out.println("invalid date");
+        }
+    }
+
+    public static boolean isLeapYear(int year) {
+        if(year % 4 == 0){
+            if (year % 100 == 0){
+                if (year % 400 == 0){
+                    return true;
+                }else {
+                    return false;
+                }
+            }else {
+                return true;
+            }
+        }else {
+            return false;
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
@@ -2,6 +2,13 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class TriangleSidesValidator {
     public void validate(double firstSide, double secondSide, double thirdSide) {
-
+        double sumOfSides = firstSide + secondSide + thirdSide;
+        switch ((int) sumOfSides){
+            case 180:
+                System.out.println("this is a valid triangle");
+                break;
+            default:
+                System.out.println("it's not a triangle");
+        }
     }
 }
